cb5efb57d070f1c5924e05946636e1ed35029467
george kamau <flexkamau646@gmail.com>
2020-02-20 18:06:36 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function myFunction(){
+    var str=prompt("Enter the word");
+    var word=str.slice(0,str.length -1);
+    var st=parseInt(str.charAt(str.length -1));
+    if(isNaN(st)){return str +1  
+    
+    }
+    
+    else {
+        word+(st+1);
+    }
+    
+    };
